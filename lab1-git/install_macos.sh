5912d4077c69da3d215e11a3fa18b2979bf3fd4a
Claudio Facchinetti <devack@hotmail.it>
2019-02-22 17:29:39 +0100

    Rimossi i file legati al sistema operativo

--- lab1-git/install_macos.sh
+++
@@ -1,6 +0,0 @@
-if ! brew --version > /dev/null; then
-	echo "Installing brew"
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-echo "Installing git"
-brew install git
