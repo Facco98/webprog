5180e7c30d5be8f297f501995dfcb71ae1d10252
Claudio Facchinetti <devack@hotmail.it>
2019-03-15 14:04:57 +0100

    Nel secondo esercizio ora si usano le annotation al posto del file web.xml

--- lab4-servlet-forms/src/ex2/Exercise2.java
+++ lab4-servlet-forms/src/ex2/Exercise2.java
@@ -1,6 +1,7 @@
 package ex2;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,13 +10,14 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.Optional;
 
+@WebServlet(name = "Exercise2", urlPatterns = "/ex2/compute")
 public class Exercise2 extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        var abilitato = !Optional.ofNullable(req.getParameter("abilitato")).isEmpty();
-        var proseguire = !Optional.ofNullable(req.getParameter("proseguire")).isEmpty();
+        var abilitato = Optional.ofNullable(req.getParameter("abilitato")).orElse("off").equals("off");
+        var proseguire = Optional.ofNullable(req.getParameter("proseguire")).orElse("off").equals("off");
         var output = new BufferedWriter( new OutputStreamWriter(resp.getOutputStream()));
         output.write("<html><head><title>Exercise 2</title></head><body><b>Valore delle checkboxes: </b><br>");
         output.write("abilitato: " + Boolean.toString(abilitato) +" </br>");
